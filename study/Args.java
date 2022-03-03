160d68eaa1132e7330fbf016afd15c4b241b3668
nodev918 <devnode77@gmail.com>
2022-03-04 03:48:30 +0800

    hh

---
+++ study/Args.java
@@ -0,0 +1,10 @@
+package study;
+
+public class Args {
+    public static void main(String[] args){
+        // Hello hello = new Hello();
+        // System.out.println(hello.sayHello());
+        System.out.println(args.length);
+    }
+
+}
---
+++ study/ReadMe.md
@@ -0,0 +1,2 @@
+$ javac -classpath C:\Users\USER\yale\coding\sp\Qproj\office\study\study ./study/*.java
+$ java study.Args hi
